a3b1ffec8eb6ce0c005f752f0e034f02c8d8f95a
Luke Allen <lukeallen@gmail.com>
2016-06-12 11:36:03 -0700

    add westco logo and tweak user flow

--- app/controllers/tasting_rooms_controller.rb
+++ app/controllers/tasting_rooms_controller.rb
@@ -3,6 +3,7 @@ class TastingRoomsController < ApplicationController
   before_action :logged_in_user, only: [:index]
   before_action :admin_user, only: [:index]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  include ActionView::Helpers::NumberHelper
 
   # GET /tasting_rooms
   # GET /tasting_rooms.json
@@ -13,64 +14,77 @@ class TastingRoomsController < ApplicationController
   # GET /tasting_rooms/1
   # GET /tasting_rooms/1.json
   def show
+
     #@start_time_this_year = Date.today.beginning_of_year
     if @tasting_room.sales_summaries.blank?
       redirect_to new_sales_summary_path(tasting_room: @tasting_room, month: Date.today.month - 1 , year: Date.today.year)
     end
     @end_month = Date.today.month - 1
     @you = Array.new
+    @you_formatted = []
     @them = []
+    @them_formatted = []
     for i in 1..@end_month do
       sales_you = @tasting_room.sales_summaries_this_year.where(month: i).first
       if sales_you.present?
         if params[:tasters].present?
           @you[i] = sales_you.num_of_tasters
-          #@them[i] = sales_you.avg_tasters
+          @you_formatted[i] = sales_you.num_of_tasters.to_s
         elsif params[:purchasers].present?
           @you[i] = sales_you.num_of_purchasers
-          #@them[i] = sales_you.avg_purchasers
+          @you_formatted[i] = sales_you.num_of_purchasers.to_s
         elsif params[:sales].present?
-          #@you[i] = "$600"
           @you[i] = sales_you.sales_in_dollars
-          #@them[i] = sales_you.avg_purchasers
+          @you_formatted[i] = number_to_currency sales_you.sales_in_dollars
         elsif params[:club_signup].present?
           @you[i] = sales_you.num_of_club_signups
-          #@them[i] = sales_you.avg_club_signups
+          @you_formatted[i] = sales_you.num_of_club_signups
         elsif params[:conversion].present?
           @you[i] = sales_you.percent_tasters_purcahased
-          #@them[i] = sales_you.avg_tasters_purchased
+          @you_formatted[i] = number_to_percentage sales_you.percent_tasters_purcahased, precision: 2
         elsif params[:club_conversion].present?
           @you[i] = sales_you.percent_club_signup
-          #@them[i] = sales_you.avg_club_conversion
+          @you_formatted[i] = number_to_percentage sales_you.percent_club_signup, precision: 2
         elsif params[:sales_per_taster].present?
           @you[i] = sales_you.sales_per_taster
-          #@them[i] = sales_you.avg_sales_per_taster
+          @you_formatted[i] = number_to_currency sales_you.sales_per_taster
         elsif params[:sales_per_purchase].present?
           @you[i] = sales_you.sales_per_purchase
+          @you_formatted[i] = number_to_currency sales_you.sales_per_purchase
         else
           @you[i] = sales_you.num_of_tasters
+          @you_formatted[i] = sales_you.num_of_tasters.to_s
         end
       else
         @you[i] = 0.0
       end
       if params[:tasters].present?
         @them[i] = @tasting_room.region.avg_tasters(i, Date.today.year)
+        @them_formatted[i] = @them[i]
       elsif params[:purchasers].present?
         @them[i] = @tasting_room.region.avg_purchasers(i, Date.today.year)
+        @them_formatted[i] = @them[i]
       elsif params[:club_signup].present?
         @them[i] = @tasting_room.region.avg_club_signups(i, Date.today.year)
+        @them_formatted[i] = @them[i]
       elsif params[:conversion].present?
         @them[i] = @tasting_room.region.avg_tasters_purchased(i, Date.today.year)
+        @them_formatted[i] = number_to_percentage @them[i], precision: 2
       elsif params[:club_conversion].present?
         @them[i] = @tasting_room.region.avg_club_conversion(i, Date.today.year)
+        @them_formatted[i] = number_to_percentage @them[i], precision: 2
       elsif params[:sales_per_taster].present?
         @them[i] = @tasting_room.region.avg_sales_per_taster(i, Date.today.year)
+        @them_formatted[i] = number_to_currency @them[i]
       elsif params[:sales_per_purchase].present?
         @them[i] = @tasting_room.region.avg_sales_per_purchase(i, Date.today.year)
+        @them_formatted[i] = number_to_currency @them[i]
       elsif params[:sales].present?
         @them[i] = @tasting_room.region.avg_sales_in_dollars(i, Date.today.year)
+        @them_formatted[i] = number_to_currency @them[i]
       else
         @them[i] = @tasting_room.region.avg_tasters(i, Date.today.year)
+        @them_formatted[i] = @them[i]
         params[:tasters] = true
       end
 
@@ -116,7 +130,7 @@ class TastingRoomsController < ApplicationController
 
     respond_to do |format|
       if @tasting_room.update(tasting_room_params)
-        
+
         format.html { redirect_to @tasting_room, notice: 'Tasting room was successfully updated.' }
         format.json { render :show, status: :ok, location: @tasting_room }
       else
